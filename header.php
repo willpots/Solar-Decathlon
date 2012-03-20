358ccb7be724fb7a1fcc8c8fa8245eb5b56e1738
Will Potter <will@wpotter.me>
2012-03-20 12:12:10 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>insite | Team Middlebury</title>
+	<meta name="author" content="" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="all" />
+	
+	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/libs/jquery.js" /></script>
+	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js" /></script>
+	<script type="text/javascript">
+	
+	</script>
+	
+</head>
+<body>
+	<div id="container">
+		<div id="main-header">
+			<div id="main-quotation">
+				<div id="quote-body">
+				"How many connections we can enable is a much more interesting question than how many kilawatt-hours we can generate."
+
+				</div>
+				<div id="quote-author">
+					 - Bill McKibben
+				</div>
+			</div>
+			<div id="main-nav">
+				<div class="nav-button"></div>
+				<div class="nav-button"></div>
+				<div class="nav-button"></div>
+				<div class="nav-button"></div>
+				<div class="nav-button"></div>
+			</div>
+		</div>
+		<div id="main-section">
+		
+		
+		
+		
+
+		

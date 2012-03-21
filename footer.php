cc0ca43791bcc8d72c427c42a4e825f74d2838b7
Will Potter <will@wpotter.me>
2012-03-21 10:54:05 -0400

    More changes to the theme.

--- footer.php
+++ footer.php
@@ -2,9 +2,12 @@
 		</div><!-- #main-section -->
 		<div id="main-footer">
 			<div id="footer-image-1" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/shelter.png" alt="Shelter" title="Shelter"/></div>
-			<div id="footer-image-2" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/house.png" alt="House" title="House"/></div>
-			<div id="footer-image-3" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/home.png" alt="Home" title="Home"/></div>
-			<div id="footer-image-4" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/community.png" alt="Community" title="Community"/></div>
+			<div id="footer-image-2" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+			<div id="footer-image-3" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/house.png" alt="House" title="House"/></div>
+			<div id="footer-image-4" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+			<div id="footer-image-5" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/home.png" alt="Home" title="Home"/></div>
+			<div id="footer-image-6" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+			<div id="footer-image-7" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/community.png" alt="Community" title="Community"/></div>
 		</div><!-- #main-footer -->
 	</div><!-- #container -->
 	<?php analytics(); ?>
--- front-page.php
+++ front-page.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
 <div id="team-picture"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/TeamPhoto.JPG" alt="Team Middlebury 2013" title="Team Middlebury 2013"></div>
-<div id="main-sidebar"></div>
+<div id="main-sidebar">
+	<div id="logo-container">
+		<img src="<?php bloginfo("stylesheet_directory"); ?>/img/logo.png" alt="insite" title="insite" />
+	</div>
+
+</div>
 <?php get_footer(); ?>

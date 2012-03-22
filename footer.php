97d05610239acd9f94df79e7f5c714fd69ab9d7c
Will Potter <will@wpotter.me>
2012-03-22 10:03:59 -0400

    Added new footer, minus facebook and twitter icons

--- footer.php
+++ footer.php
@@ -1,14 +1,29 @@
 
 		</div><!-- #main-section -->
 		<div id="main-footer">
-			<div id="footer-image-1" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/shelter.png" alt="Shelter" title="Shelter"/></div>
-			<div id="footer-image-2" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
-			<div id="footer-image-3" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/house.png" alt="House" title="House"/></div>
-			<div id="footer-image-4" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
-			<div id="footer-image-5" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/home.png" alt="Home" title="Home"/></div>
-			<div id="footer-image-6" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
-			<div id="footer-image-7" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/community.png" alt="Community" title="Community"/></div>
+			<div id="shelter-structure">
+				<div id="footer-image-1" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/shelter.png" alt="Shelter" title="Shelter"/></div>
+				<div id="footer-image-2" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+				<div id="footer-image-3" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/house.png" alt="House" title="House"/></div>
+				<div id="footer-image-4" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+				<div id="footer-image-5" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/home.png" alt="Home" title="Home"/></div>
+				<div id="footer-image-6" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/arrow.png" alt="" title=""/></div>
+				<div id="footer-image-7" class="footer-image"><img src="<?php bloginfo("stylesheet_directory"); ?>/img/schematics/community.png" alt="Community" title="Community"/></div>
+			</div><!-- #shelter-structure -->
 		</div><!-- #main-footer -->
+		<div id="logos-footer">
+			<div id="logos">
+				<a href="http://www.middlebury.edu">
+					<img src="<?php bloginfo("stylesheet_directory"); ?>/img/midd.png" alt="Middlebury College" title="Middlebury College" >
+				</a>
+				<a href="http://www.nrel.gov/">
+					<img src="<?php bloginfo("stylesheet_directory"); ?>/img/nrel2.png" alt="NREL" title="NREL" >
+				</a>
+				<a href="http://www.solardecathlon.gov/">
+					<img src="<?php bloginfo("stylesheet_directory"); ?>/img/USDOE.png" alt="USDOE" title="USDOE" >
+				</a>
+			</div>
+		</div>
 	</div><!-- #container -->
 	<?php analytics(); ?>
 </body>
